cd2f371e593c614234e2900861bf9646b738cce7
Rocky Li <aperocky@gmail.com>
2019-04-17 20:55:23 -0400

    Add multithreading to article crawling

--- crawling/reuters.py
+++ crawling/reuters.py
@@ -1,24 +1,11 @@
 # GET REUTERS SOURCE.
-from bs4 import BeautifulSoup
-import requests # apparently reuters doesn't need selenium.
 import sys
 sys.path.append("..") # To run main test.
-from utils import NewsObject
+from utils import NewsObject, get_soup
+from multiprocessing.pool import ThreadPool
 
 REUTER_MARKET = "https://www.reuters.com/finance/markets"
-header = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0. 2357.134 Safari/537.36"}
-
-def get_soup(url):
-    r = requests.get(url, headers=header)
-    soup = BeautifulSoup(r.content, "html.parser")
-    return soup
-
-def get_content(url):
-    r = requests.get(url, headers=header)
-    soup = BeautifulSoup(r.content, "html.parser")
-    article = soup.select(".StandardArticleBody_body > p")
-    paragraphs = [p.text for p in article]
-    return paragraphs
+REUTER_HOME = "https://www.reuters.com"
 
 def reuter_extract_headlines(soup):
     # Return a list of NewsObject
@@ -27,10 +14,9 @@ def reuter_extract_headlines(soup):
     try:
         top_story = soup.select(".topStory h2 a")[0]
         title = top_story.text
-        href = REUTER_MARKET + top_story["href"]
+        href = REUTER_HOME + top_story["href"]
         summary = soup.select(".topStory > p")[0].text
-        content = get_content(href)
-        contents = NewsObject.packContent(content=content, summary=summary)
+        contents = NewsObject.packContent(summary=summary)
         top = NewsObject(title, href, contents)
         headlines.append(top)
     except Exception as e:
@@ -42,10 +28,9 @@ def reuter_extract_headlines(soup):
         try:
             sub = each.select("a")[0]
             title = sub.text
-            href = REUTER_MARKET + sub["href"]
+            href = REUTER_HOME + sub["href"]
             timestamp = each.select(".timestamp")[0].text
-            content = get_content(href)
-            contents= NewsObject.packContent(content=content, timestamp=timestamp)
+            contents= NewsObject.packContent(timestamp=timestamp)
             top = NewsObject(title, href, contents)
             headlines.append(top)
         except Exception as e:
@@ -57,17 +42,28 @@ def reuter_extract_blocks(soup, areaid):
     contents = soup.select("#{} .story-content".format(areaid))
     for each in contents:
         try:
-            href = REUTER_MARKET + each.select("a")[0]["href"]
+            href = REUTER_HOME + each.select("a")[0]["href"]
             title = each.select("h3")[0].text.strip()
             summary = each.select("p")[0].text
             timestamp = each.select(".timestamp")[0].text
-            content = get_content(href)
-            contents = NewsObject.packContent(summary=summary, timestamp=timestamp, content=content)
+            contents = NewsObject.packContent(summary=summary, timestamp=timestamp)
             news = NewsObject(title, href, contents)
             area_news.append(news)
         except Exception as e:
             print("Error in getting {} data: {}".format(areaid, e))
     return area_news
+
+def get_content(url):
+    soup = get_soup(url)
+    article = soup.select(".StandardArticleBody_body > p")
+    paragraphs = [p.text for p in article]
+    return paragraphs
+
+def add_content_to_news(newsobj):
+    href = newsobj.href
+    print("RUNNING THREADPOOL")
+    content = get_content(href)
+    newsobj.contents["content"] = content
 
 # process the page REUTER_MARKET.
 def run():
@@ -77,11 +73,14 @@ def run():
     blocks = ["tab-markets-us", "tab-markets-emea", "tab-markets-asia"]
     for each in blocks:
         master_list.extend(reuter_extract_blocks(soup, each))
+    pool = ThreadPool(processes=20) # 20 threads at each time.
+    pool.map(add_content_to_news, master_list)
     return master_list
 
 if __name__ == "__main__":
+    import time
+    starttime = time.time()
     newslist = run()
-    for e in newslist:
-        print(e.title)
-        print(e.href)
-        print(e.contents["summary"])
+    elapsed = time.time() - starttime
+    print("TIME ELAPSED: {};    ARTICLES GOT: {}".format(elapsed, len(newslist)))
+    print("\n\n".join([str(e) for e in newslist]))
--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
-# Define common data structures.
+# Define common function and data structures.
 import time
+import requests
+from bs4 import BeautifulSoup
+
+header = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0. 2357.134 Safari/537.36"}
+
+def get_soup(url):
+    r = requests.get(url, headers=header)
+    soup = BeautifulSoup(r.content, "html.parser")
+    return soup
 
 class NewsObject:
 
@@ -21,6 +30,14 @@ class NewsObject:
         if kwargs:
             contents.update(kwargs)
         return contents
+
+    def __str__(self):
+        contents = ["timestamp", "summary", "content"]
+        contentstr = ["\n".join(self.contents[e]) if e == "content" else
+                      self.contents[e] for e in contents]
+        selfstr = [self.title, self.href] + contentstr
+        selfstr = [e for e in selfstr if e is not None]
+        return "\n".join(selfstr)
 
     def calculate_weights(self):
         # TODO: somehow calculate an importance.
